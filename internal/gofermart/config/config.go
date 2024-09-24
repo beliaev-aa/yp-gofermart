c7d4c247f8511a98754b10ab334de61d865839b3
Beliaev Aleksei <38virtual@gmail.com>
2024-09-24 09:47:19 +0300

    master рефакторинг инициализации конфигурации приложения

--- internal/gofermart/config/config.go
+++ internal/gofermart/config/config.go
@@ -4,50 +4,41 @@ import (
 	"beliaev-aa/yp-gofermart/internal/gofermart/domain"
 	"flag"
 	"github.com/caarlos0/env/v10"
-	"strings"
 )
 
-// parseFlags - парсит флаги и возвращает конфигурацию
-func parseFlags() *domain.Config {
-	cfg := &domain.Config{}
-	flag.StringVar(&cfg.AccrualSystemAddress, "r", "http://localhost:8080", "Address of the accrual settlement system")
-	flag.StringVar(&cfg.DatabaseURI, "d", "", "PostgreSQL DSN")
-	flag.StringVar(&cfg.RunAddress, "a", "localhost:8080", "Address and port to run the HTTP server")
-	flag.StringVar(&cfg.JWTSecret, "s", "your-256-bit-secret-key", "Your JWT-secret key")
-	flag.Parse()
-	return cfg
-}
+const (
+	defaultRunAddress           = "localhost:8080"
+	defaultDatabaseURI          = ""
+	defaultAccrualSystemAddress = "http://localhost:8080"
+	defaultJWTSecret            = "your-256-bit-secret-key"
+)
 
 // LoadConfig - загружает конфигурацию, отдает приоритет переменным окружения
 func LoadConfig() (*domain.Config, error) {
-	// Загружаем переменные окружения
-	envConfig := &domain.Config{}
-	err := env.Parse(envConfig)
-	if err != nil {
+	runAddressFlag := flag.String("a", defaultRunAddress, "Address and port to run the HTTP server")
+	databaseURIFlag := flag.String("d", defaultDatabaseURI, "PostgreSQL DSN")
+	accrualSystemAddressFlag := flag.String("r", defaultAccrualSystemAddress, "Address of the accrual settlement system")
+	jwtSecretFlag := flag.String("s", defaultJWTSecret, "Your JWT-secret key")
+
+	flag.Parse()
+
+	cfg := domain.Config{}
+	if err := env.Parse(&cfg); err != nil {
 		return nil, err
 	}
 
-	// Загружаем флаги командной строки
-	flagConfig := parseFlags()
-
-	// Создаем финальную конфигурацию, выбирая значения с приоритетом у переменных окружения
-	finalConfig := &domain.Config{
-		RunAddress:           selectCfgFromSource(flagConfig.RunAddress, envConfig.RunAddress),
-		DatabaseURI:          selectCfgFromSource(flagConfig.DatabaseURI, envConfig.DatabaseURI),
-		AccrualSystemAddress: selectCfgFromSource(flagConfig.AccrualSystemAddress, envConfig.AccrualSystemAddress),
-		JWTSecret:            selectCfgFromSource(flagConfig.JWTSecret, envConfig.JWTSecret),
+	if cfg.RunAddress == defaultRunAddress {
+		cfg.RunAddress = *runAddressFlag
+	}
+	if cfg.DatabaseURI == defaultDatabaseURI {
+		cfg.DatabaseURI = *databaseURIFlag
+	}
+	if cfg.AccrualSystemAddress == defaultAccrualSystemAddress {
+		cfg.AccrualSystemAddress = *accrualSystemAddressFlag
+	}
+	if cfg.JWTSecret == defaultJWTSecret {
+		cfg.JWTSecret = *jwtSecretFlag
 	}
 
-	return finalConfig, nil
-}
-
-// selectCfgFromSource - выбирает значение из двух источников
-func selectCfgFromSource(flagValue, envValue string) string {
-	envValue = strings.TrimSpace(envValue)
-	flagValue = strings.TrimSpace(flagValue)
-
-	if envValue != "" {
-		return envValue
-	}
-	return flagValue
+	return &cfg, nil
 }
--- internal/gofermart/config/config_test.go
+++ internal/gofermart/config/config_test.go
@@ -8,174 +8,91 @@ import (
 	"testing"
 )
 
-func TestSelectCfgFromSource(t *testing.T) {
-	type testCase struct {
-		name          string
-		flagValue     string
-		envValue      string
-		expectedValue string
-	}
-
-	testCases := []testCase{
-		{
-			name:          "Env_value_has_priority",
-			flagValue:     "flag-value",
-			envValue:      "env-value",
-			expectedValue: "env-value",
-		},
-		{
-			name:          "Flag_value_is_used_when_env_is_empty",
-			flagValue:     "flag-value",
-			envValue:      "",
-			expectedValue: "flag-value",
-		},
-		{
-			name:          "Empty_flag_and_env",
-			flagValue:     "",
-			envValue:      "",
-			expectedValue: "",
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			result := selectCfgFromSource(tc.flagValue, tc.envValue)
-			if result != tc.expectedValue {
-				t.Errorf("expected %v, got %v", tc.expectedValue, result)
-			}
-		})
-	}
-}
-
 func TestLoadConfig(t *testing.T) {
 	type testCase struct {
 		name           string
-		flagConfig     *domain.Config
-		envConfig      *domain.Config
+		envVariables   map[string]string
+		args           []string
 		expectedConfig *domain.Config
-	}
-
-	mockEnvParse := func(envConfig *domain.Config) {
-		*envConfig = domain.Config{
-			RunAddress:           "env-run-address",
-			DatabaseURI:          "env-database-uri",
-			AccrualSystemAddress: "env-accrual-address",
-			JWTSecret:            "env-jwt-secret",
-		}
-	}
-
-	mockFlagConfig := &domain.Config{
-		RunAddress:           "flag-run-address",
-		DatabaseURI:          "flag-database-uri",
-		AccrualSystemAddress: "flag-accrual-address",
-		JWTSecret:            "flag-jwt-secret",
-	}
-
-	expectedConfig := &domain.Config{
-		RunAddress:           "env-run-address",
-		DatabaseURI:          "env-database-uri",
-		AccrualSystemAddress: "env-accrual-address",
-		JWTSecret:            "env-jwt-secret",
+		expectedErr    bool
 	}
 
 	testCases := []testCase{
 		{
-			name:           "Env values have priority",
-			flagConfig:     mockFlagConfig,
-			envConfig:      &domain.Config{},
-			expectedConfig: expectedConfig,
+			name: "Env_Variables_Have_Priority_Over_Flags",
+			envVariables: map[string]string{
+				"RUN_ADDRESS":            "env_run_address",
+				"DATABASE_URI":           "env_database_uri",
+				"ACCRUAL_SYSTEM_ADDRESS": "env_accrual_system_address",
+				"JWT_SECRET":             "env_jwt_secret",
+			},
+			args: []string{
+				"-a", "flag_run_address",
+				"-d", "flag_database_uri",
+				"-r", "flag_accrual_system_address",
+				"-s", "flag_jwt_secret",
+			},
+			expectedConfig: &domain.Config{
+				RunAddress:           "env_run_address",
+				DatabaseURI:          "env_database_uri",
+				AccrualSystemAddress: "env_accrual_system_address",
+				JWTSecret:            "env_jwt_secret",
+			},
+			expectedErr: false,
+		},
+		{
+			name:         "Flags_Used_If_Env_Variables_Not_Set",
+			envVariables: map[string]string{},
+			args: []string{
+				"-a", "flag_run_address",
+				"-d", "flag_database_uri",
+				"-r", "flag_accrual_system_address",
+				"-s", "flag_jwt_secret",
+			},
+			expectedConfig: &domain.Config{
+				RunAddress:           "flag_run_address",
+				DatabaseURI:          "flag_database_uri",
+				AccrualSystemAddress: "flag_accrual_system_address",
+				JWTSecret:            "flag_jwt_secret",
+			},
+			expectedErr: false,
+		},
+		{
+			name:         "Default_Values_Used_If_Neither_Flags_Or_Env_Set",
+			envVariables: map[string]string{},
+			args:         []string{},
+			expectedConfig: &domain.Config{
+				RunAddress:           "localhost:8080",
+				DatabaseURI:          "",
+				AccrualSystemAddress: "http://localhost:8080",
+				JWTSecret:            "your-256-bit-secret-key",
+			},
+			expectedErr: false,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			mockEnvParse(tc.envConfig)
+			os.Clearenv()
+			os.Args = append([]string{"cmd"}, tc.args...)
 
-			config := &domain.Config{
-				RunAddress:           selectCfgFromSource(tc.flagConfig.RunAddress, tc.envConfig.RunAddress),
-				DatabaseURI:          selectCfgFromSource(tc.flagConfig.DatabaseURI, tc.envConfig.DatabaseURI),
-				AccrualSystemAddress: selectCfgFromSource(tc.flagConfig.AccrualSystemAddress, tc.envConfig.AccrualSystemAddress),
-				JWTSecret:            selectCfgFromSource(tc.flagConfig.JWTSecret, tc.envConfig.JWTSecret),
+			for key, value := range tc.envVariables {
+				if err := os.Setenv(key, value); err != nil {
+					t.Fatalf("failed to set env variable: %s", key)
+				}
 			}
 
-			if diff := cmp.Diff(tc.expectedConfig, config); diff != "" {
+			flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+
+			cfg, err := LoadConfig()
+
+			if (err != nil) != tc.expectedErr {
+				t.Errorf("expected error: %v, got: %v", tc.expectedErr, err)
+			}
+
+			if diff := cmp.Diff(tc.expectedConfig, cfg); diff != "" {
 				t.Fatalf("Unexpected config (-want +got):\n%s", diff)
 			}
 		})
-	}
-}
-
-func TestParseFlags(t *testing.T) {
-	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-
-	os.Args = []string{"cmd", "-r", "http://test-accrual", "-d", "test-dsn", "-a", "localhost:9000", "-s", "test-secret"}
-
-	expected := &domain.Config{
-		AccrualSystemAddress: "http://test-accrual",
-		DatabaseURI:          "test-dsn",
-		RunAddress:           "localhost:9000",
-		JWTSecret:            "test-secret",
-	}
-
-	config := parseFlags()
-
-	if diff := cmp.Diff(expected, config); diff != "" {
-		t.Fatalf("Unexpected config (-want +got):\n%s", diff)
-	}
-}
-
-func TestLoadConfigArgs(t *testing.T) {
-	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-
-	err := os.Setenv("RUN_ADDRESS", "env-run-address")
-	if err != nil {
-		return
-	}
-	err = os.Setenv("DATABASE_URI", "env-database-uri")
-	if err != nil {
-		return
-	}
-	err = os.Setenv("ACCRUAL_SYSTEM_ADDRESS", "env-accrual-address")
-	if err != nil {
-		return
-	}
-	err = os.Setenv("JWT_SECRET", "env-jwt-secret")
-	if err != nil {
-		return
-	}
-
-	os.Args = []string{"cmd", "-r", "http://flag-accrual", "-d", "flag-dsn", "-a", "localhost:9000", "-s", "flag-secret"}
-
-	expected := &domain.Config{
-		RunAddress:           "env-run-address",
-		DatabaseURI:          "env-database-uri",
-		AccrualSystemAddress: "env-accrual-address",
-		JWTSecret:            "env-jwt-secret",
-	}
-
-	config, err := LoadConfig()
-	if err != nil {
-		t.Fatalf("LoadConfig returned an error: %v", err)
-	}
-
-	if diff := cmp.Diff(expected, config); diff != "" {
-		t.Fatalf("Unexpected config (-want +got):\n%s", diff)
-	}
-
-	err = os.Unsetenv("RUN_ADDRESS")
-	if err != nil {
-		return
-	}
-	err = os.Unsetenv("DATABASE_URI")
-	if err != nil {
-		return
-	}
-	err = os.Unsetenv("ACCRUAL_SYSTEM_ADDRESS")
-	if err != nil {
-		return
-	}
-	err = os.Unsetenv("JWT_SECRET")
-	if err != nil {
-		return
 	}
 }
